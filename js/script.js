dff3725286dba98a0b25c9c302eafdd561405681
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-10 14:38:11 +0100

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Stampare a schermo un messaggio all’interno di un h1,
+utilizzando i data. */
+
+var app = new Vue ({
+    el: "#titolo",
+    data: {
+        title : "Alex Del Piero",
+        immagine : "https://static.fanpage.it/wp-content/uploads/sites/27/2020/11/del-piero-fine-rapporto-juventus-agnelli-1604702234462-1200x1200.jpg",
+        regolaImg : "dimensioni"
+    }
+})
